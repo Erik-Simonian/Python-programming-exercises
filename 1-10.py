f231f1b13ecd5af9f42bc8d5233c0836c9d0199c
Erik Simonian <ericsim90210@gmail.com>
2023-03-23 18:55:01 +0500

    Added rough versions of two more answers.Clean up later

--- 1-10.py
+++ 1-10.py
@@ -28,6 +28,7 @@ def answer1():
 
 
 print(answer1())
+
 # ---------------------------------------------------------
 
 """Question 2:
@@ -49,3 +50,51 @@ def answer2(n):
 # print(answer2(user_input))
 num = 8                                 # You can change "num" value the factorial 
 print(answer2(num))                     # of a desired number
+
+# -----------------------------------------------------
+
+"""Question 3:
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is 
+an integral number between 1 and n (both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}"""
+
+
+def answer3(n):
+    result = {}                                     # result = dict() also works here
+    for i in range(1, n+1):
+        result[i] = i * i
+    return result
+
+
+print(answer3(8))
+# print(answer3(input))
+
+# ----------------------------------------------------
+
+
+"""Question 4:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple 
+which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')"""
+
+
+def answer4(n):
+    n_list = [n]
+    tuple_split = n.split(", ")
+    n_tup = tuple(tuple_split)
+    return n_list, n_tup
+
+
+print(answer4("34,67,55,33,12,98"))
+# print(answer4(input()))
+
+
+
+
