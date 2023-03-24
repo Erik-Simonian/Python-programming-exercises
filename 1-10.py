a59d861036098ed8c2cee79a78282f810596df75
Erik Simonian <ericsim90210@gmail.com>
2023-03-24 21:02:48 +0500

    Added two more questions.Cleaned up a little

--- 1-10.py
+++ 1-10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 """Question 1:
 Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
@@ -16,7 +17,7 @@ def answer1_np():                           # Numpy Version
     return result
 
 
-print(answer1_np())
+# print(answer1_np())
 
 
 def answer1():
@@ -27,7 +28,8 @@ def answer1():
     return result
 
 
-print(answer1())
+# print(answer1())
+# ---------------------------------------------------------
 
 # ---------------------------------------------------------
 
@@ -49,7 +51,8 @@ def answer2(n):
 # user_input = int(input())             # Uncomment for and input option
 # print(answer2(user_input))
 num = 8                                 # You can change "num" value the factorial 
-print(answer2(num))                     # of a desired number
+# print(answer2(num))                     # of a desired number
+# -----------------------------------------------------
 
 # -----------------------------------------------------
 
@@ -69,11 +72,11 @@ def answer3(n):
     return result
 
 
-print(answer3(8))
+# print(answer3(8))
 # print(answer3(input))
-
 # ----------------------------------------------------
 
+# ----------------------------------------------------
 
 """Question 4:
 Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple 
@@ -92,9 +95,58 @@ def answer4(n):
     return n_list, n_tup
 
 
-print(answer4("34,67,55,33,12,98"))
+# print(answer4("34,67,55,33,12,98"))
 # print(answer4(input()))
+# -----------------------------------------------------
+
+# -----------------------------------------------------
+
+"""Question 5:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods."""
 
 
+class AnswerFive:
+    def __init__(self):
+        self.msg = None
+
+    def getString(self):
+        self.msg = input()
+
+    def printString(self):
+        print(self.msg.upper())
 
 
+# obj = AnswerFive()
+# obj.getString()
+# obj.printString()
+# -------------------------------------------------------
+
+# -------------------------------------------------------
+"""Question 6:
+Write a program that calculates and prints the value according to the given formula:
+Q = Square root of [(2 * C * D)/H]
+Following are the fixed values of C and H:
+C is 50. H is 30.
+D is the variable whose values should be input to your program in a comma-separated sequence.
+Example
+Let us assume the following comma separated input sequence is given to the program:
+100,150,180
+The output of the program should be:
+18,22,24"""
+
+
+def answer_6():
+    c = 50
+    h = 30
+    result = []
+    item_list = [i for i in input().split(",")]
+    for item in item_list:
+        result.append(str(int(round(math.sqrt(2 * c * float(item) / h)))))
+
+    print(",".join(result))
+
+
+answer_6()
