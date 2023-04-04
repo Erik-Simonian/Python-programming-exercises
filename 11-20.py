0025502d6483a4c6eeef9b61403daa2bb8c1f8be
Erik Simonian <ericsim90210@gmail.com>
2023-04-04 23:03:32 +0500

    Edited question 16

--- 11-20.py
+++ 11-20.py
@@ -123,5 +123,21 @@ def answer_15(n, r=4):                                      # Recursion version 
     return sum(answer_15_dict.values())
 
 
-print(answer_15(9))
+# print(answer_15(9))
+# ----------------------------------------
 
+# ----------------------------------------
+"""Question 16:
+Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+Suppose the following input is supplied to the program:
+1,2,3,4,5,6,7,8,9
+Then, the output should be:
+1,3,5,7,9"""
+
+
+def answer_16():
+    result = [x for x in input().split(",") if int(x) % 2 != 0]
+    return ",".join(result)
+
+
+# print(answer_16())
