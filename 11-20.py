986c85a56ae37ab32381e7ef0d70de02afd9afe0
Erik Simonian <ericsim90210@gmail.com>
2023-04-13 21:06:17 +0500

    Added question 19

--- 11-20.py
+++ 11-20.py
@@ -207,3 +207,36 @@ def answer_18():
 
 
 # answer_18()
+# ----------------------------------------------------
+
+# ----------------------------------------------------
+"""Question:
+You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, 
+age and height are numbers. The tuples are input by console. The sort criteria is:
+1: Sort based on name;
+2: Then sort based on age;
+3: Then sort by score.
+The priority is that name > age > score.
+If the following tuples are given as input to the program:
+Tom,19,80
+John,20,90
+Jonny,17,91
+Jonny,17,93
+Json,21,85
+Then, the output of the program should be:
+[('John', '20', '90'), ('Jonny', '17', '91'), ('Jonny', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]"""
+
+
+def answer_19():
+    from operator import itemgetter
+    result = []
+    while True:
+        msg = input()
+        if not msg:
+            break
+        result.append((tuple(msg.split(','))))
+
+    print(sorted(result, key=itemgetter(0, 1, 2)))
+
+
+# answer_19()
