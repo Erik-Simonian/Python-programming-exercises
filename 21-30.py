0f80ce28dba18201daf215a78bca4d8601a13cee
Erik Simonian <ericsim90210@gmail.com>
2023-04-17 23:36:23 +0500

    Added question 22

--- 21-30.py
+++ 21-30.py
@@ -40,7 +40,42 @@ def answer_21():
             position[1] += distance
         else:
             print("Please insert your command in a 'DIRECTION STEPS' format "
-                  "or leave it blank of you want to get the result")
+                  "or leave it blank if you want to get the result")
 
 
-print(answer_21())
+# print(answer_21())
+# ----------------------------------------------------
+
+# ----------------------------------------------------
+"""Question 22:
+Write a program to compute the frequency of the words from the input. 
+The output should output after sorting the key alphanumerically. 
+Suppose the following input is supplied to the program:
+New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
+Then, the output should be:
+2:2
+3.:1
+3?:1
+New:1
+Python:5
+Read:1
+and:1
+between:1
+choosing:1
+or:2
+to:1
+"""
+
+
+def answer_22():
+    msg = input().split()
+    dict_result = {}
+    for i in msg:
+        dict_result[i] = dict_result.get(i, 0) + 1
+    sorted_dict = sorted(dict_result.items())
+    for element in sorted_dict:
+        result = ','.join(str(item) for item in element)
+        print(result)
+
+
+# answer_22()
