24d1b37d14603fb5a2f5df1f9f6a90203b1af07d
Erik Simonian <ericsim90210@gmail.com>
2023-04-18 04:40:55 +0500

    Added questions 23 and 24

--- 21-30.py
+++ 21-30.py
@@ -79,3 +79,36 @@ def answer_22():
 
 
 # answer_22()
+# ----------------------------------------------
+
+# ----------------------------------------------
+"""Question 23:
+    Write a method which can calculate square value of number
+"""
+
+
+def answer_23(n):
+    return n ** 2
+
+
+# print(answer_23(3))
+# ----------------------------------------------
+
+# ----------------------------------------------
+"""Question 24:
+    Python has many built-in functions, and if you do not know how to use it, 
+    you can read document online or find some books. 
+    But Python has a built-in document function for every built-in functions.
+    Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+    And add document for your own function"""
+
+
+# print(abs.__doc__)
+# print(int.__doc__)
+# print(input.__doc__)
+def answer_24(n):
+    """Return square value of n."""
+    return n ** 2
+
+
+# print(answer_24.__doc__)
