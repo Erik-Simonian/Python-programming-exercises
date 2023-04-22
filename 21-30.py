eabb1e963bc075b5ead7568d08038b76267df0b1
Erik Simonian <ericsim90210@gmail.com>
2023-04-22 18:33:26 +0500

    Added questions 29 and 30

--- 21-30.py
+++ 21-30.py
@@ -170,3 +170,33 @@ def answer_28(n, m):
 
 
 # answer_28("6", "5")
+# -------------------------------------
+
+# -------------------------------------
+"""Question 29:
+Define a function that can accept two strings as input and concatenate them and then print it in console."""
+
+
+def answer_29(str1, str2):
+    print(str1 + str2)
+
+
+# --------------------------------------------
+
+# --------------------------------------------
+"""Question 30:
+Define a function that can accept two strings as input and print the string with maximum length in console. 
+If two strings have the same length, then the function should print all strings line by line."""
+
+
+def answer_30(str1, str2):
+    if len(str1) > len(str2):
+        print(str1)
+    elif len(str2) > len(str1):
+        print(str2)
+    else:
+        print(str1)
+        print(str2)
+
+
+# answer_30("Example One", "Example Number Two")
