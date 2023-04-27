60daf0db2e9753b26588f8f1c598d0a8787b31a3
Erik Simonian <ericsim90210@gmail.com>
2023-04-27 23:53:00 +0500

    Added questions 37-40

--- 31-40.py
+++ 31-40.py
@@ -95,3 +95,53 @@ def answer_36():
 # --------------------------------------------
 
 # --------------------------------------------
+"""Question 37:
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). 
+Then the function needs to print the first 5 elements in the list."""
+
+
+def answer_37():
+    result = [i ** 2 for i in range(1, 21)]
+    print(result[:5])
+
+
+# answer_37()
+# ---------------------------------------------
+
+# ---------------------------------------------
+"""Question 38:
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). 
+Then the function needs to print the last 5 elements in the list."""
+
+
+def answer_38():
+    result = [i ** 2 for i in range(1, 21)]
+    print(result[-5:])
+
+
+# answer_38()
+# ----------------------------------------------
+
+# ----------------------------------------------
+"""Question 39
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). 
+Then the function needs to print all values except the first 5 elements in the list."""
+
+
+def answer_39():
+    result = [i ** 2 for i in range(1, 21)]
+    print(result[5:])
+
+
+# answer_39()
+# ----------------------------------------------
+
+# ----------------------------------------------
+"""Question 40
+Define a function which can generate and print a tuple 
+where the value are square of numbers between 1 and 20 (both included). """
+
+
+def answer_40():
+    result = [i ** 2 for i in range(1, 21)]
+    print(tuple(result))
