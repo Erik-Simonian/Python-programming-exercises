e4e4481f2a6326365932247904cc3c0aee3b1500
Erik Simonian <ericsim90210@gmail.com>
2023-05-26 16:12:54 +0500

    Added questions 90

--- 81-90.py
+++ 81-90.py
@@ -120,6 +120,17 @@ def answer_89():
 # -------------------------------------------------------
 
 # -------------------------------------------------------
+"""Question 90:
+With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list
+whose elements are intersection of the above given lists.
+Use set() and "&=" to do set intersection operation."""
 
 
+def answer_90():
+    set_1 = {1, 3, 6, 78, 35, 55}
+    set_2 = {12, 24, 35, 24, 88, 120, 155}
+    set_1 &= set_2
+    return list(set_1)
 
+
+# print(answer_90())
